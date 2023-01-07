06d3c9323d7b00a856ccd6b9cb36be27b6802c9d
daguitosama <tocarralero@gmail.com>
2023-01-06 20:28:01 -0500

    content: description

--- components/TheHomeIntro.vue
+++ components/TheHomeIntro.vue
@@ -5,11 +5,17 @@
         <!-- pic -->
         <TheHomeIntroPic />
         <!-- heading -->
-        <div class="mt-[45px]">
+        <div class="mt-[45px] text-center">
             <MainHeading text="Hi I'm Dago" />
         </div>
 
         <!-- description -->
-        <div></div>
+        <div class="mt-[20px]">
+            <p class="text-center font-medium text-xl">
+                I’m a young web contractor. I like to handcraft functional and
+                unique web experiences, tailored to my client and his clients
+                needs.
+            </p>
+        </div>
     </section>
 </template>
