ad002bc1d3112727e621d4c76041f79ab876d99d
daguitosama <tocarralero@gmail.com>
2023-01-06 20:25:45 -0500

    feat: page transitions

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    css: ["~/assets/css/fonts.css"],
+    css: ["~/assets/css/main.css", "~/assets/css/fonts.css"],
     modules: ["@nuxtjs/i18n", "@nuxtjs/color-mode", "@nuxtjs/tailwindcss"],
-
+    app: {
+        pageTransition: { name: "page", mode: "out-in" },
+    },
     i18n: {
         // loca
     },
