cb12836096a1034674dcf3e7b83266694af24fe0
daguitosama <tocarralero@gmail.com>
2023-01-08 09:54:01 -0500

    feat: latest jobs in work

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 
 <template>
-    <div class="mx-auto max-w-md pt-[120px]">
+    <div class="mx-auto max-w-md pt-[120px] px-[30px]">
         <SectionContact />
     </div>
 </template>
--- pages/work/index.vue
+++ pages/work/index.vue
@@ -2,8 +2,9 @@
 
 <template>
     <div
-        class="flex flex-col justify-center pb-[200px] mx-auto max-w-screen-lg w-full px-[30px]"
+        class="flex flex-col justify-center pb-[200px] pt-[120px] mx-auto max-w-screen-lg w-full px-[30px] gap-[50px]"
     >
         <SectionWork />
+        <SectionProjects />
     </div>
 </template>
