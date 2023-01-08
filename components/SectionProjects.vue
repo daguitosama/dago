019e4861e2b3dc22ccc6f53886537be65d703d99
daguitosama <tocarralero@gmail.com>
2023-01-08 11:37:49 -0500

    clean: not used props removal

--- components/SectionProjects.vue
+++ components/SectionProjects.vue
@@ -4,7 +4,7 @@
     <div class="mx-auto max-w-screen-xl w-full">
         <MainHeading text="Latest Jobs" :useH2="true" />
         <div class="mt-[50px] flex flex-col items-center sm:items-start">
-            <WorkCardList :workCards="workCards" />
+            <WorkCardList />
         </div>
     </div>
 </template>
