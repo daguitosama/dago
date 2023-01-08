fe0f53a4b1cec910058842e905aec2894494c5e6
daguitosama <tocarralero@gmail.com>
2023-01-08 09:43:20 -0500

    feat: allow using h1 or h2

--- components/global/MainHeading.vue
+++ components/global/MainHeading.vue
@@ -4,9 +4,25 @@ const props = defineProps({
         type: String,
         default: "Main Heading",
     },
+    useH1: {
+        type: Boolean,
+        default: true,
+    },
+    useH2: {
+        type: Boolean,
+        default: false,
+    },
 });
 </script>
 
 <template>
-    <h1 class="font-bold text-5xl md:text-6xl lg:text-7xl">{{ text }}</h1>
+    <h1
+        v-if="useH1 && !useH2"
+        class="font-bold text-5xl md:text-6xl lg:text-7xl"
+    >
+        {{ text }}
+    </h1>
+    <h2 v-else-if="useH2" class="font-bold text-5xl md:text-6xl lg:text-7xl">
+        {{ text }}
+    </h2>
 </template>
