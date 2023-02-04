9efb48644d8918359bbd2976f839237ea37606d5
daguitosama <tocarralero@gmail.com>
2023-02-04 15:38:13 -0500

    + metadata

--- pages/about.vue
+++ pages/about.vue
@@ -1,4 +1,69 @@
-<script setup></script>
+<script setup>
+    const { public: BASE_URL } = useRuntimeConfig();
+    useHead({
+        title: "Dago AC",
+        meta: [
+            // description
+            {
+                hid: "description",
+                name: "description",
+                content: "Web design & Web development",
+            },
+            // OG / Facebook
+            {
+                hid: "og:type",
+                property: "og:type",
+                content: "website",
+            },
+            {
+                hid: "og:url",
+                property: "og:url",
+                content: `${BASE_URL}/about`,
+            },
+            {
+                hid: "og:title",
+                property: "og:title",
+                content: "Dago AC",
+            },
+            {
+                hid: "og:description",
+                property: "og:description",
+                content: "Web design & Web development",
+            },
+            {
+                hid: "og:image",
+                property: "og:image",
+                content: `${BASE_URL}/img/global/og.jpg`,
+            },
+            // Twitter
+            {
+                hid: "twitter:card",
+                property: "twitter:card",
+                content: "summary_large_image",
+            },
+            {
+                hid: "twitter:url",
+                property: "twitter:url",
+                content: `${BASE_URL}/about`,
+            },
+            {
+                hid: "twitter:title",
+                property: "twitter:title",
+                content: "Dago AC",
+            },
+            {
+                hid: "twitter:description",
+                property: "twitter:description",
+                content: "Web design & Web development",
+            },
+            {
+                hid: "twitter:image",
+                property: "twitter:image",
+                content: `${BASE_URL}/img/global/og.jpg`,
+            },
+        ],
+    });
+</script>
 
 <template>
     <section
@@ -13,7 +78,10 @@
                 class="absolute inset-0 w-full h-full object-cover"
             />
         </div>
-        <MainHeading text="About Me" class="mt-[45px] text-center" />
+        <MainHeading
+            text="About Me"
+            class="mt-[45px] text-center"
+        />
         <div class="mt-[45px] w-full font-medium max-w-xs">
             <div class="text-center text-xl">
                 <p>
